0116d8628e296418cca5c7844845579acba8c781
sinheung kim <s2120@e-mirim.hs.kr>
2023-04-20 14:07:18 +0900

    초기 커밋.

---
+++ ConsoleApp31/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            while(x<50)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(x, 5);
+
+
+                if(x % 3 == 0)
+                {
+                    Console.WriteLine("__@");
+                }else if (x % 3 == 1)
+                {
+                    Console.WriteLine("_^@");
+                }
+                else
+                {
+                    Console.WriteLine("^-@");
+                }
+                Thread.Sleep(1000);
+                x++;
+            }
+        }
+    }
+}
