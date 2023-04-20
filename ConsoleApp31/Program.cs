53fa8a3e03e935324b5095e64678f2a88dff4324
sinheung kim <s2120@e-mirim.hs.kr>
2023-04-20 14:56:29 +0900

    변수 정의

--- ConsoleApp31/Program.cs
+++ ConsoleApp31/Program.cs
@@ -9,27 +9,36 @@ namespace ConsoleApp31
 {
     internal class Program
     {
+        private const int CHAR_STATE_TOTAL = 3;
+        private const int CHAR_STATE1 = 0;
+        private const int CHAR_STATE2 = 1;
+        private const int CHAR_STATE3 = 2;
+        private const int ROW = 5;
+        private const int COL_START = 1;
+        private const int COL_END = 50;
+        private const int WAIT_TIME = 1000;
+
         static void Main(string[] args)
         {
             int x = 1;
-            while(x<50)
+            while(x< COL_END)
             {
                 Console.Clear();
                 Console.SetCursorPosition(x, 5);
 
-                switch(x % 3)
+                switch(x % CHAR_STATE_TOTAL)
                 {
-                    case 0:
+                    case CHAR_STATE1:
                         Console.WriteLine("__@");
                         break;
-                    case 1:
+                    case CHAR_STATE2:
                         Console.WriteLine("_^@");
                         break;
-                    case 2:
+                    case CHAR_STATE3:
                         Console.WriteLine("^_@");
                         break;
                 }
-                Thread.Sleep(1000);
+                Thread.Sleep(WAIT_TIME);
                 x++;
             }
         }
